41a72878da11ac0de73935d69658153bd453cedb
Resurt <mr.roman-07@ya.ru>
2021-02-26 12:20:35 +0300

    Планирование – залог успеха - 2

--- schema.sql
+++ schema.sql
@@ -6,57 +6,48 @@ USE taskforce_db;
 
 CREATE TABLE `user` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` CHAR(50) NOT NULL, UNIQUE (`email`),
-  `email` CHAR(50) NOT NULL,
+  `name` VARCHAR(50) NOT NULL, UNIQUE (`email`),
+  `email` VARCHAR(50) NOT NULL,
   `birthday` DATE NOT NULL,
-  `phone` INT(11) NULL,
-  `skype` CHAR(50) NULL,
-  `telegram` CHAR(50) NULL,
+  `phone` VARCHAR(50) NULL,
+  `skype` VARCHAR(50) NULL,
+  `telegram` VARCHAR(50) NULL,
   `about` text NOT NULL,
-  `password` CHAR(64) NOT NULL, PRIMARY KEY (`id`),
+  `password` VARCHAR(64) NOT NULL, PRIMARY KEY (`id`),
   `city_id` INT(11) NOT NULL,
   `role_id` INT(11) NOT NULL
 ) ENGINE = InnoDB;
 
 CREATE TABLE `city` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` CHAR(150) NOT NULL
-) ENGINE=InnoDB;
-
-CREATE TABLE `role` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` INT(11) NOT NULL
+  `name` VARCHAR(150) NOT NULL,
+  `ltd` POINT NULL,
+  `lgd` POINT NULL
 ) ENGINE=InnoDB;
 
 CREATE TABLE `category` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` CHAR(100) NOT NULL
-) ENGINE=InnoDB;
-
-CREATE TABLE `status` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` CHAR(100) NOT NULL,
+  `name` VARCHAR(100) NOT NULL
 ) ENGINE=InnoDB;
 
 CREATE TABLE `task` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `title` CHAR(100) NOT NULL,
-  `details` CHAR(250) NOT NULL,
+  `title` VARCHAR(100) NOT NULL,
+  `details` VARCHAR(250) NOT NULL,
   `category_id` INT(11) NOT NULL,
-  `link` CHAR(100) NULL,
+  `link` VARCHAR(100) NULL,
   `city_id` INT(11) NULL,
-  `ltd` POINT NULL,
-  `lgd` POINT NULL,
   `status_id` INT(11) NOT NULL,
   `price_task` INT UNSIGNED  NULL,
   `deadline` DATE NULL,
   `user_id` INT(11) NOT NULL
+  `executor_id` INT(11) NOT NULL
 ) ENGINE=InnoDB;
 
 CREATE TABLE `response` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `price_offer` INT UNSIGNED NULL,
-  `comment` VARCHAR(250) NULL,
+  `comment` TEXT NULL,
   `user_id` INT(11) NOT NULL,
   `task_id` INT(11) NOT NULL
 ) ENGINE=InnoDB;
@@ -70,7 +61,7 @@ CREATE TABLE `completing_task` (
 
 CREATE TABLE `status_completed` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` CHAR(10) NOT NULL
+  `name` VARCHAR(10) NOT NULL
 ) ENGINE=InnoDB;
 
 CREATE TABLE `message` (
